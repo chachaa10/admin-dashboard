682074baa47c6c1e6fa55418a7c5c4de95b047e3
Chalex <177287665+chalexmarmol@users.noreply.github.com>
2025-06-14 15:19:06 +0800

    feat: add closing sidebar function on click outside and escape

--- src/main.js
+++ src/main.js
@@ -1,24 +1,38 @@
-const toggleSidebar = document.getElementById('toggle-sidebar');
+const sidebarToggle = document.getElementById('toggle-sidebar');
+const sidebar = document.getElementById('sidebar');
+const rightContent = document.querySelector('.right');
+const dashboardTitle = document.getElementById('dashboard-title');
+const spanTitles = document.querySelectorAll('.flex-opt span');
 
-function openSidebar() {
-  const sidebar = document.getElementById('sidebar');
+function toggleSidebar() {
   sidebar.classList.toggle('open');
 
-  const dashboardTitle = document.getElementById('dashboard-title');
-  dashboardTitle.classList.toggle('hidden');
+  const isNowOpen = sidebar.classList.contains('open');
 
-  const spanTitleS = document.querySelectorAll('.flex-opt span');
-  spanTitleS.forEach((span) => {
-    span.classList.toggle('hidden');
+  dashboardTitle.classList.toggle('hidden', !isNowOpen);
+
+  spanTitles.forEach((span) => {
+    span.classList.toggle('hidden', !isNowOpen);
   });
+
+  rightContent.classList.toggle('hidden', isNowOpen);
 }
 
-function hideContent() {
-  const right = document.querySelector('.right');
-  right.classList.toggle('hidden');
+function closeSidebarOutside(event) {
+  if (sidebar.classList.contains('open') && !sidebar.contains(event.target) && !sidebarToggle.contains(event.target)) {
+    toggleSidebar();
+  }
 }
 
-toggleSidebar.addEventListener('click', () => {
-  openSidebar();
-  hideContent();
-});
+function closeSidebarEscape(event) {
+  if (event.key === 'Escape' && sidebar.classList.contains('open')) {
+    toggleSidebar();
+  }
+}
+
+// Open and close the sidebar
+sidebarToggle.addEventListener('click', toggleSidebar);
+
+// Close sidebar
+document.addEventListener('click', closeSidebarOutside);
+document.addEventListener('keydown', closeSidebarEscape);
