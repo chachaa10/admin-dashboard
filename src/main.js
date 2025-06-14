ff037940123e6f537f0652bd8743ed1d3cc312de
Chalex <177287665+chalexmarmol@users.noreply.github.com>
2025-06-14 16:07:37 +0800

    feat: add opening sidebar when clicking links

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ const sidebar = document.getElementById('sidebar');
 const rightContent = document.querySelector('.right');
 const dashboardTitle = document.getElementById('dashboard-title');
 const spanTitles = document.querySelectorAll('.flex-opt span');
+const links = document.querySelectorAll('.flex-opt');
 
 function toggleSidebar() {
   sidebar.classList.toggle('open');
@@ -30,9 +31,25 @@ function closeSidebarEscape(event) {
   }
 }
 
-// Open and close the sidebar
+// Open and close the sidebar when the toggle button is clicked
 sidebarToggle.addEventListener('click', toggleSidebar);
 
-// Close sidebar
+// Close sidebar when clicking outside the sidebar or pressing Escape
 document.addEventListener('click', closeSidebarOutside);
 document.addEventListener('keydown', closeSidebarEscape);
+
+if (window.matchMedia('(min-width: 768px)').matches) {
+  if (!sidebar.classList.contains('open')) {
+    toggleSidebar();
+  }
+}
+
+// --- Link Click Behavior ---
+links.forEach((link) => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (!sidebar.classList.contains('open')) {
+      toggleSidebar();
+    }
+  });
+});
