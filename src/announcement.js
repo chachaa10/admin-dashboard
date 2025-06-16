a9582a17d9af3a59d022528f25f7e1e2856dca49
Chalex <177287665+chalexmarmol@users.noreply.github.com>
2025-06-16 11:03:09 +0800

    feat: add announcement

---
+++ src/announcement.js
@@ -0,0 +1,27 @@
+const announcements = [
+  {
+    title: 'Purr-fectly Important: Site Maintenance!',
+    description:
+      "Our human helpers are tidying things up! We'll be back to chasing laser pointers (and serving you) soon!",
+  },
+  {
+    title: 'Meow-gical Community Share Day!',
+    description: "Come share your favorite toys and sunbeam spots with us! It'll be the cat's pajamas!",
+  },
+  {
+    title: 'New Squeaky Toy (Privacy Policy)!',
+    description: "We've updated our rules for napping and snuggling. Take a peek if you're curious!",
+  },
+];
+
+for (const { title, description } of announcements) {
+  const announcementList = document.createElement('li');
+  announcementList.classList.add('announcement-list');
+  announcementList.innerHTML = `
+    <h2>${title}</h2>
+    <p>${description}</p>
+  `;
+
+  const announcementCard = document.querySelector('.announcement-card');
+  announcementCard.appendChild(announcementList);
+}
